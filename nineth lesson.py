e8f7d065a2af0b2008d44aa5db4dce737c8e9ddd
Lecking <2722702142@qq.com>
2022-11-11 18:58:51 +0800

    1.1

--- nineth lesson.py
+++ nineth lesson.py
@@ -1,3 +1,67 @@
-list=('a','b','c')
-for i in list:
+names=['张赫','张三','小红','老六']
+cars=['BMW','honda','BYD','benz']
+wants=['李四',"王五","冯七"]
+print(names[0],names[1],names[2],names[3])
+for i in names:
     print(i)
+for i in range(0,4):
+    print("早上好",names[i])
+for i in range(0,4):
+    print(names[i]+'乘坐',cars[i])
+for i in wants:
+    print(i+'请与我共进晚餐')
+print(names[0]+"无法来")
+print(wants[0]+"可以来")
+names[0]=wants[0]
+for i in names:
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
